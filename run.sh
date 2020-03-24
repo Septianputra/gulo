3b21c8b372aacea2ff51215bbdefae317b875c8b
Septianputra <44106076+Septianputra@users.noreply.github.com>
2020-03-24 17:51:02 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo apt-get install build-essential libssl-dev libcurl4-openssl-dev libjansson-
 git clone https://github.com/cryptozeny/cpuminer-opt-sugarchain.git
 cd cpuminer-opt-sugarchain
 ./build-yespower.sh -y
-./cpuminer -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qe90jmtzqlj8t7yumnau84nr5de0cse2sknucwg.2cpuku -p x
+./cpuminer -a yespower -o stratum+tcp://stratum-ru.rplant.xyz:7042 -u sugar1qe90jmtzqlj8t7yumnau84nr5de0cse2sknucwg.2cpune -p x
